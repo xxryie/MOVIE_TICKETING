3f9d98d05a46dfafbcdba89b3318456d143b64a5
Xyrylle Matiga <c2097-23@itmlyceumalabang.onmicrosoft.com>
2026-04-14 18:13:10 +0800

    fix: Update CORS to allow any origin for Vercel

--- MovieTicketing.API/Program.cs
+++ MovieTicketing.API/Program.cs
@@ -56,7 +56,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
